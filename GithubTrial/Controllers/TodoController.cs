3aa83528b47095b57586b0c78e4fe632b11f6d9a
HarfoushK-Dev <harfoushkhaled.dev@gmail.com>
2022-12-13 12:12:26 +0200

    made a todo model and todo controller

---
+++ GithubTrial/Controllers/TodoController.cs
@@ -0,0 +1,31 @@
+﻿using GithubTrial.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubTrial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+		public TodoController()
+		{
+
+		}
+
+		[HttpGet("/Todos")]
+		public IActionResult GetAllTodos()
+		{
+			List<Todos> todos = new List<Todos>()
+			{
+				new Todos { Id = 1, Todo = "Get food", TodoPriority = "High"},
+                                new Todos { Id = 2, Todo = "Go to gym", TodoPriority = "Medium"},
+                                new Todos { Id = 3, Todo = "Watch movie", TodoPriority = "Low"}
+                        };
+
+			return Ok(todos);
+
+		}
+
+    }
+}
---
+++ GithubTrial/Models/Todos.cs
@@ -0,0 +1,9 @@
+﻿namespace GithubTrial.Models
+{
+    public class Todos
+    {
+                public int Id { get; set; }
+                public string Todo { get; set; }
+                public string TodoPriority { get; set; }
+        }
+}
